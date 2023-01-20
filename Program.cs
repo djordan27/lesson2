f75df4402bcf0219247b3c8187f8a5a42362ded5
Игорь <211353@mail.ru>
2023-01-20 19:15:57 +0300

    finish

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        void Run(int run)
+        {
+            string run1 = Convert.ToString(run);
+            Console.WriteLine(run1);
+
+        }
+
+    Run(10);
+    }
+}
